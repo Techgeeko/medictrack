591577a0225c151c47f48a4de1a4fbe836cd726f
Techgeeko <agulonyemicheal9@gmail.com>
2025-05-10 16:03:15 +0100

    Initial commit

--- components/home/_components/header.tsx
+++ components/home/_components/header.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image"
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
-import { Navbar } from "./navbar"
 
 export function Header() {
   return (
