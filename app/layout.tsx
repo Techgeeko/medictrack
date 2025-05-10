1237d6670597997daa138c2e3da2a28e6beec62b
Techgeeko <agulonyemicheal9@gmail.com>
2025-05-10 18:33:21 +0100

    Updated Opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,28 @@ export const metadata: Metadata = {
   description: "MedicTrack is an all-in-one clinic management software for medical practitioners to book appointments, manage patient records, send automated reminders, and reduce no-shows. Simplify your practice and enhance patient care. HIPAA-compliant",
   icons: {
     icon: '/favicon.ico'
-  }
+  },
+openGraph: {
+    title: "MedicTrack | Patient Appointment Booking & Medical Practice Management Software",
+    description: "MedicTrack is an all-in-one clinic management software for medical practitioners to book appointments, manage patient records, send automated reminders, and reduce no-shows. Simplify your practice and enhance patient care. HIPAA-compliant.",
+    url: "https://medictrack.vercel.app",
+    siteName: "MedicTrack",
+    images: [
+      {
+        url: "/og.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "MedicTrack | Patient Appointment Booking & Medical Practice Management Software",
+    description: "MedicTrack is an all-in-one clinic management software for medical practitioners to book appointments, manage patient records, send automated reminders, and reduce no-shows. Simplify your practice and enhance patient care. HIPAA-compliant.",
+    images: '/og.png', // Local image
+  },
 };
 
 export default function RootLayout({
